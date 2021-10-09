e3d2391d28c978fbe6e72a6b546382ba1e672b96
sman40901 <sujanmanpr@gmail.com>
2021-10-08 21:02:47 -0700

    back-end added

---
+++ back-end/src/index.js
@@ -0,0 +1,13 @@
+const { GraphQLServer } = require('graphql-yoga')
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: ""
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
